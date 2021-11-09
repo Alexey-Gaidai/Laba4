4ccdd5c175920b10096fc10bf2a942124b5ee412
Alexey-Gaidai <71748686+Alexey-Gaidai@users.noreply.github.com>
2021-11-09 16:40:02 +0700

    начало положено

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Laba4
 {
     public partial class Form1 : Form
     {
+        public static List<double> steps = new List<double>();//список точек
+        public double[] sss;
+
+        private static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        private const string SpreadsheetId = "1Kcvpqi-I6wY0HSFGehgdVp_tS70Fk2KQroZT39Z8S5Q";
+        private const string GoogleCredentialsFileName = "google-credentials.json";
+        private const string ReadRange = "Лист1!A:A";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private static SheetsService GetSheetsService()//получаем ответ от сервера
+        {
+            using (var stream = new FileStream(GoogleCredentialsFileName, FileMode.Open, FileAccess.Read))
+            {
+                var serviceInitializer = new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = GoogleCredential.FromStream(stream).CreateScoped(Scopes)
+                };
+                return new SheetsService(serviceInitializer);
+            }
+        }
+
+        private async Task ReadAsync(SpreadsheetsResource.ValuesResource valuesResource)//выполняем чтение
+        {
+            var response = await valuesResource.Get(textBox2.Text, ReadRange).ExecuteAsync();
+            var values = response.Values;
+            if (values == null || !values.Any())
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+            sss = new double[values.Count];
+            for (int i = 0; i < values.Count; i++)
+            {
+                double val0 = Convert.ToDouble(values[i][0]);
+                steps.Add(val0);
+                sss[i] = val0;
+            }
+
+        }
+
+        async private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var serviceValues = GetSheetsService().Spreadsheets.Values;
+                await ReadAsync(serviceValues);
+                for (int i = 0; i < steps.Count; i++)
+                {
+                    dataGridView1.Rows.Add(sss[i]);
+                }
+                BuildChart(0, steps.Count+1, 1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void BuildChart(double x_min, double x_max, double dx)
+        {
+            Chart chart1 = new Chart();
+            groupBox1.Controls.Add(chart1);
+            ChartArea area = new ChartArea();
+            area.AxisX.Minimum = x_min;
+            area.AxisX.Maximum = x_max;
+            area.AxisX.MajorGrid.Enabled = true;
+            chart1.ChartAreas.Add(area);
+            Series series1 = new Series();
+            series1.ChartType = SeriesChartType.Column;
+            chart1.Series.Add(series1);
+            for (int i = 0; i < steps.Count; i++)
+                chart1.Series[0].Points.Add(sss[i]);//отрисовка
         }
     }
 }
